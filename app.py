a72d3e09ce6bc4f2ecbdf8b18b73739ae4e64b1b
GodotWorker <132081339+GodotWorker@users.noreply.github.com>
2025-09-24 12:38:59 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -1,13 +1,6 @@
 from flask import Flask
-
-# Create an instance of the Flask class
 app = Flask(__name__)
 
-# Define a route for the root URL ("/")
-@app.route("/")
+@app.route('/')
 def hello_world():
-    return "<p>Hello, World!</p>"
-
-# Run the application if the script is executed directly
-if __name__ == "__main__":
-    app.run(debug=True) # debug=True enables debugging features
+    return 'Hello, World!'
